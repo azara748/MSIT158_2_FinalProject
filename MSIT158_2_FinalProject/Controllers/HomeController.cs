8118f8c64653a850cd51bcf04d48d5bf6c39c1d9
史昆靈 <k955339962@gmail.com>
2024-06-06 23:25:12 +0800

    完成基礎登入

--- MSIT158_2_FinalProject/Controllers/HomeController.cs
+++ MSIT158_2_FinalProject/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using MSIT158_2_FinalProject.Models;
+using MSIT158_2_FinalProject.ViewModel;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace MSIT158_2_FinalProject.Controllers
 {
@@ -19,8 +21,60 @@ namespace MSIT158_2_FinalProject.Controllers
         {
             //var a = _context.TOrders.FirstOrDefault();
             //return Json(a);
-           return View();
+            if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGIN_MEMBER))
+            return View();
+
+                return RedirectToAction("Login");
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(CLoginViewModel vm)
+        {
+            TMember user = _context.TMembers.FirstOrDefault(
+                t => t.EMail.Equals(vm.txtEmail) && t.Password.Equals(vm.txtPassword));
+
+            if (user != null && user.Password.Equals(vm.txtPassword))
+            {
+                string json = JsonSerializer.Serialize(user);
+                HttpContext.Session.SetString(CDictionary.SK_LOGIN_MEMBER, json);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult BackIndex()
+        {
+            if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGIN_MEMBER))
+                return View();
+            return RedirectToAction("BackLogin");
+        }
+
+        public IActionResult BackLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BLogin(CLoginViewModel vm)
+        {
+            TEmployee emp = _context.TEmployees.FirstOrDefault(
+                t => t.EMail.Equals(vm.txtEmail) && t.Password.Equals(vm.txtPassword));
+
+
+            if (emp != null && emp.Password.Equals(vm.txtPassword))
+            {
+                string json = JsonSerializer.Serialize(emp);
+                HttpContext.Session.SetString(CDictionary.SK_LOGIN_MEMBER, json);
+
+                return RedirectToAction("BIndex");
+            }
+            return View();
+        }
+
+
 
         public IActionResult Privacy()
         {
---
+++ MSIT158_2_FinalProject/Models/CDictionary.cs
@@ -0,0 +1,8 @@
+﻿namespace MSIT158_2_FinalProject.Models
+{
+    public class CDictionary
+    {
+        public static readonly string SK_UNPAIED_ROOMS_LIST = "SK_UNPAIED_ROOMS_LIST";
+        public static readonly string SK_LOGIN_MEMBER = "SK_LOGIN_MEMBER";
+    }
+}
