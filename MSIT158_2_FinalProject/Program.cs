172e9f00f1aea8d2600dcad0cd23dd6488fdffca
史昆靈 <k955339962@gmail.com>
2024-06-10 15:20:46 +0800

    員工照片顯示成功

--- MSIT158_2_FinalProject/Program.cs
+++ MSIT158_2_FinalProject/Program.cs
@@ -51,6 +51,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=BackLogin}/{id?}");
+    pattern: "{controller=Home}/{action=Login}/{id?}");
 
 app.Run();
