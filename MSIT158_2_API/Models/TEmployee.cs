1f3c88ac97357a9360130d474e9bc7b0647e967c
史昆靈 <k955339962@gmail.com>
2024-06-11 23:24:26 +0800

    #Google 登入完成

--- MSIT158_2_API/Models/TEmployee.cs
+++ MSIT158_2_API/Models/TEmployee.cs
@@ -6,35 +6,25 @@ using System.ComponentModel.DataAnnotations;
 namespace MSIT158_2_API.Models;
 
 public partial class TEmployee
-{
-    [Display(Name = "序號")]
+{    
     public int EmployeeId { get; set; }
 
-    [Display(Name = "員工姓名")]
     public string? EmployeeName { get; set; }
 
-    [DisplayName("手機")]
     public string? Cellphone { get; set; }
 
-    [DisplayName("地址")]
     public string? Address { get; set; }
 
-    [DisplayName("生日")]
     public string? Birthday { get; set; }
 
-    [DisplayName("密碼")]
     public string? Password { get; set; }
 
-    [DisplayName("電子信箱")]
     public string? EMail { get; set; }
 
-    [DisplayName("員工照片")]
     public byte[]? EmployeePhoto { get; set; }
 
-    [DisplayName("上班日")]
     public string? OnBoardDate { get; set; }
 
-    [DisplayName("主管序號")]
     public int? DepId { get; set; }
 
     public virtual TDepartment? Dep { get; set; }
