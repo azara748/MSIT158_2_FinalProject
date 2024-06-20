4433512e217baec9e24fd46edc39905d1fb5eac8
Lu <2186enya@gmail.com>
2024-06-20 20:01:27 +0800

    0620 active update

--- MSIT158_2_FinalProject/Controllers/ActiveController.cs
+++ MSIT158_2_FinalProject/Controllers/ActiveController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using MSIT158_2_FinalProject.Models;
 
 namespace MSIT158_2_FinalProject.Controllers
@@ -9,7 +11,28 @@ namespace MSIT158_2_FinalProject.Controllers
         {
             return View();
         }
-         public IActionResult Create()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TActive active, IFormFile myimg)
+        {
+            if (ModelState.IsValid)
+            {
+                if (myimg != null)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        myimg.CopyTo(ms);
+                        active.ActiveImage = ms.ToArray();
+                    }
+                }
+                _context.Add(active);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Categories"] = new SelectList(_context.Set<TActive>(), "Id", "Name", active.ActiveId);
+            return View(active);
+        }
+        public IActionResult Create()
         {
             return View();
         }
@@ -29,7 +52,7 @@ namespace MSIT158_2_FinalProject.Controllers
             TActive a = db.TActives.FirstOrDefault(x => x.ActiveId == id);
             if (a == null)
                 return RedirectToAction("Index");
-            return View();
+            return View(a);
         }
         [HttpPost]
         public IActionResult Edit(TActive a)
@@ -61,6 +84,45 @@ namespace MSIT158_2_FinalProject.Controllers
                 }
             }
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase File)
+        {
+            if (File != null && File.ContentLength > 0)
+            {
+                //存到資料夾
+                var FileName = Path.GetFileName(File.FileName);
+                var FilePath = Path.Combine(Server.MapPath("~/Images/"), FileName);
+                File.SaveAs(FilePath);
+
+
+                //轉成byte 方法一 直接轉
+                byte[] FileBytes;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    File.InputStream.CopyTo(ms);
+                    FileBytes = ms.GetBuffer();
+                }
+                //方法二 讀實體檔案出來再轉
+                //using (var Fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+                //{
+                //    using (var Reader = new BinaryReader(Fs))
+                //    {
+                //        FileBytes = Reader.ReadBytes((int)Fs.Length);
+                //    }
+                //}
+
+                //存進資料庫再取出
+                InsertPhoto(FileBytes);
+                FileBytes = SelectPhoto();
+
+                TempData["Data"] = FileBytes;
+
+            }
+
+
+            return View();
         }
     }
 }
---
+++ MSIT158_2_FinalProject/ViewModel/CActiveImageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MSIT158_2_FinalProject.ViewModel
+{
+    public partial class CActiveImageViewModel
+    {
+        public int ImageId {  get; set; }
+        public byte[] ActiveImage { get; set; }
+    }
+}
