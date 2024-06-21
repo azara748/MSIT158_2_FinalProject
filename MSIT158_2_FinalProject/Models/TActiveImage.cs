ab2b9a7781d81db36dec9c422e641327f9866207
31 <wanks894@gmail.com>
2024-06-21 15:54:02 +0800

    0621

---
+++ MSIT158_2_FinalProject/Models/TActiveImage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSIT158_2_FinalProject.Models;
+
+public partial class TActiveImage
+{
+    public int ImageId { get; set; }
+
+    public byte[] ActiveImage { get; set; } = null!;
+}
